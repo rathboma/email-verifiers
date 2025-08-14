c4a629c1961ffe9baeeefbfc7a650d1912f0ec54
Matthew Rathbone <matthew.rathbone@gmail.com>
2025-08-14 14:15:29 -0500

    fix cron

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 set -euxo pipefail
+source ~/.bashrc
+export GIT_SSH_COMMAND='ssh -i /home/rathboma/.ssh/id_rsa -o IdentitiesOnly=yes -o UserKnownHostsFile=/home/rathboma/.ssh/known_hosts -o StrictHostKeyChecking=yes'
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/.."
 
 git pull
-bin/automate.sh
+/home/rathboma/.local/bin/mise exec node@18 -- bin/automate.sh
